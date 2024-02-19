721a847055f3c462903571283349d5aeb3c345e8
thesajidahmad <sajidmdpur@gmail.com>
2024-02-20 02:49:47 +0530

    Try to Create random mobile number using JavaScript

---
+++ randomMobNo.js
@@ -0,0 +1,7 @@
+// console.log(`9${Math.floor(Math.random()*1000000000)}`);
+const number = (`9${Math.floor(Math.random()*1000000000)}`);
+if (number.length==10) {
+  console.log(number);
+} else {
+  console.log("not a valid number");
+}
