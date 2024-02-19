2ac2eb0f3bd59dbd81e1739bb0ec65197434d8a5
thesajidahmad <sajidmdpur@gmail.com>
2024-02-19 12:48:04 +0530

    revision and practice

---
+++ basic.js
@@ -0,0 +1,27 @@
+// let a = "Sajid";
+// a="Ahmad";
+
+// console.log(a);
+
+// const b="Sajid Ahmad";
+// b="Raza";
+
+// console.log(b);
+
+
+// Array
+// const myArray = ["Sajid", "Priyanshu", "Makhdum"];
+// console.log(myArray[1]);
+
+// Objects
+// const myObj = {
+//   Name:"Sajid",
+//   Age:19
+// }
+// console.log(myObj);
+
+
+// Function
+// const myFunction = function (){
+//   console.log("Hello World")
+// }
