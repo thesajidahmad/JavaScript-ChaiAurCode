b921536a8212699094bbe77df5c99faf20a43847
thesajidahmad <sajidmdpur@gmail.com>
2024-02-19 16:57:10 +0530

    uncomment

--- basic.js
+++ basic.js
@@ -1,27 +1,27 @@
-// let a = "Sajid";
-// a="Ahmad";
+let a = "Sajid";
+a="Ahmad";
 
-// console.log(a);
+console.log(a);
 
-// const b="Sajid Ahmad";
-// b="Raza";
+const b="Sajid Ahmad";
+b="Raza";
 
-// console.log(b);
-
-
-// Array
-// const myArray = ["Sajid", "Priyanshu", "Makhdum"];
-// console.log(myArray[1]);
-
-// Objects
-// const myObj = {
-//   Name:"Sajid",
-//   Age:19
-// }
-// console.log(myObj);
+console.log(b);
 
 
-// Function
-// const myFunction = function (){
-//   console.log("Hello World")
-// }
+Array
+const myArray = ["Sajid", "Priyanshu", "Makhdum"];
+console.log(myArray[1]);
+
+Objects
+const myObj = {
+  Name:"Sajid",
+  Age:19
+}
+console.log(myObj);
+
+
+Function
+const myFunction = function (){
+  console.log("Hello World")
+}
