205d2647bc236bd606c94785f050cd8fda66ddf3
thesajidahmad <sajidmdpur@gmail.com>
2024-02-21 12:08:35 +0530

    change the code for getting random mobile number, use of function and for loop in this code

--- randomMobNo.js
+++ randomMobNo.js
@@ -1,7 +1,12 @@
-// console.log(`9${Math.floor(Math.random()*1000000000)}`);
-const number = (`9${Math.floor(Math.random()*1000000000)}`);
-if (number.length==10) {
-  console.log(number);
-} else {
-  console.log("not a valid number");
+const min = 100000000
+const max = 999999999
+
+function myFunction(){
+  console.log((`9${Math.floor(Math.random()*(max - min + 1) + min)}`));
+}
+
+
+for (let count = 0; count < 10; count++) {
+  myFunction()
+  
 }
